e594af77005e7981c5aff0992a26aa204b940aa5
Trần Trung Kiên <105615602+khieuthank@users.noreply.github.com>
2023-04-23 09:54:36 +0700

    10h/23/4

---
+++ WEB_VIA/src/java/dao/DAO.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import entity.Account;
+import entity.DBContext;
+import entity.Product;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author trung
+ */
+public class DAO {
+    
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public Account login(String user, String pass) {
+        String query = "  select * from Account \n"
+                + "  where [user] =?\n"
+                + "  and [pass] = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public Account checkAccountExist(String user) {
+        String query = "  select * from Account \n"
+                + "  where [user] =?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public void singup(String user, String pass) {
+        String query = "insert into Account values (?,?,0,0)";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public List<Product> gettAllProduct() {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            rs =ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+//    public static void main(String[] args) {
+//        DAO dao = new DAO();
+//        List<Product> list = dao.gettAllProduct();
+//        for (Product s : list) {
+//            System.out.println(s);
+//        }
+//
+//    }
+}
---
+++ WEB_VIA/src/java/entity/DBContext.java
@@ -0,0 +1,63 @@
+
+package entity;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBContext {
+    
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+    /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+     public Connection getConnection()throws Exception {
+        String url = "jdbc:sqlserver://"+serverName+":"+portNumber + "\\" + instance +";databaseName="+dbName;
+        if(instance == null || instance.trim().isEmpty())
+            url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }   
+    /*Insert your other code right after this comment*/
+    /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
+    private final String serverName = "NHUNNHUN";
+    private final String dbName = "WEb_Via";
+    private final String portNumber = "1433";
+    private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final String userID = "sa";
+    private final String password = "123";
+    
+    
+    
+       public static void main(String[] args) {
+      Connection conn = null;
+      String url = "jdbc:sqlserver://NHUNNHUN:1433;databaseName=WEb_Via";
+      String user = "sa";
+      String password = "123";
+      
+      try {
+         // Thiết lập kết nối tới SQL Server
+         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+         conn = DriverManager.getConnection(url, user, password);
+         
+         // Kiểm tra kết nối thành công hay không
+         if (conn != null) {
+            System.out.println("Kết nối tới SQL Server thành công");
+         }
+      } catch (SQLException e) {
+         System.out.println("Lỗi kết nối tới SQL Server: " + e.getMessage());
+      } catch (ClassNotFoundException e) {
+         System.out.println("Không tìm thấy Driver: " + e.getMessage());
+      } finally {
+         try {
+            if (conn != null) {
+               conn.close();
+            }
+         } catch (SQLException e) {
+            System.out.println("Lỗi đóng kết nối: " + e.getMessage());
+         }
+      }
+   }
+}
+
+ 
